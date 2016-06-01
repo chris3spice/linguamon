9a6ab1e2f8ca4a768f8e7c1f49f6753c06bab5e5
chris3spice <chrismorlang@gmail.com>
2016-05-31 23:07:53 -0800

    Delete lingua.py

--- lingua.py
+++
@@ -1,9 +0,0 @@
-""" Example Linguamon
-[[1, 2, 3], "name", ["word", "incorrect 1", "incorrect 2", "incorrect 3"], "word 2", "word 3", "word 4"]
-"""
-
-
-ling_list = [
-    [[1, 2, 3], "개구리", ["녹색", "green", "red", "pink", "blue"], ["뒤다", "jump", "kick", "fight", "fly"]],
-    [[1, 2], "개구리", ["녹색", "green"], ["뒤다", "jump"]]
-]
