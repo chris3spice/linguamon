99b0ae2c087aaa30195ec7392ebb18c9844fad3f
chris3spice <chrismorlang@gmail.com>
2016-05-31 23:14:35 -0800

    Update lingdict.py

--- data/components/lingdict.py
+++ data/components/lingdict.py
@@ -1,6 +1,6 @@
 # Dictionary for all the linguamon
 
 linguamon_list = [
-    [[1, 2, 3], "개구리", ["녹색", "green", "red", "pink", "blue"], ["뒤다", "jump", "kick", "fight", "fly"]],
-    [[1, 2], "램프", ["고양이", "cat", "dog", "elephant", "hippo"], ["열쇠", "key", "remote", "lock", "door"]],
+    [[1, 2, 3], ["개구리", "frog"], ["녹색", "green", "red", "pink", "blue"], ["뒤다", "jump", "kick", "fight", "fly"]],
+    [[1, 2], ["램프", "lamp"], ["고양이", "cat", "dog", "elephant", "hippo"], ["열쇠", "key", "remote", "lock", "door"]],
 ]
